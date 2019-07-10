c573ca6d7db650e2b519ab928f79fb81a422230a
Maksym Pidlisnyi <p.maxsym@gmail.com>
2019-07-10 12:05:38 +0300

    Forming first connection

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,7 +1,35 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientHandler {
+
+    private static Socket socket;
 
     public static void main(String[] args) {
 
+        connect();
+        end();
+
+    }
+
+    private static void connect() {
+        try {
+            socket = new Socket("localhost", 8080);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void handle() {
+
+    }
+
+    private static void end() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
